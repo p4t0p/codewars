ec2fd689eed46baa9b975a4d85e140575c50061e
admin <admin@MacBook-Pro-admin.local>
2019-08-03 17:46:37 +0200

    0.3-0.8complete

--- execrices/03-08.py
+++ execrices/03-08.py
@@ -3,15 +3,27 @@ l = [1, 10, 30, 12, 5, 0, -3, 18, 4]
 # 1
 # Вывести в консоль все элементы списка l больше n
 def print_all_bigger(n):
-    pass
+    for i in l:
+        if n < i:
+            print (i)
+    
 
 # 2
 # Вывести в консоль все нечетные элементы списка l
-def print_all_odd(n):
-    pass
+def print_all_odd(l):
+    for i in l:
+        if i%2 == 1:
+            print (i)
+    
 
 
 m = ['1', 10, 30, None, 'Hello', 0, -3, '18', 4, 5.12]
 
 # 3
 # написать функцию, которая фильтрует список выкивывая из него все, что не является числом
+def filtr_notnum(list):
+    nums = []  
+    for i in list:
+        if type (i) is int or type (i) is float:
+            nums.append(i)
+    return nums
