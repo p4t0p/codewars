d3281e772f49246c1db53508b2db9903046919a9
Trdat Mkrtchyan <t.mkrtchyan@atol.ru>
2019-08-11 14:45:03 +0300

    Added new exercise

---
+++ execrices/11-08.py
@@ -0,0 +1,19 @@
+# dicts in python
+
+d = {
+    'key_name': 'Key Value'
+}
+
+# get key's value
+d['key_name'] # 'Key Value'
+d.get('key_name') # 'Key Value'
+
+# set key value
+d['another_key'] = 500;
+
+
+def generate_user(name, age):
+    pass
+
+def filter_correct_ages(users, age = 16):
+    pass
