926cf82b7893c777f522ba65feaff233e96fbcb0
admin <admin@MacBook-Pro-admin.local>
2019-08-11 14:50:35 +0200

    rabota s dict

--- execrices/11-08.py
+++ execrices/11-08.py
@@ -13,7 +13,23 @@ d['another_key'] = 500;
 
 
 def generate_user(name, age):
-    pass
-
+    user = {
+        'name':name,
+        'age':age
+    }
+    return user
+    
+users = [
+    generate_user('John', 12),
+    generate_user('Dave', 22),
+    generate_user('Oleg', 42),
+    generate_user('Tim', 31),
+    generate_user('Kianu', 15),
+]
 def filter_correct_ages(users, age = 16):
-    pass
+    result = []
+    for i in users:
+        if i.get('age') >= age:
+            result.append(i)
+            
+    return result
