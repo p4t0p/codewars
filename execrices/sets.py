2205a0235cf363f223b463a5eb9d553c1f9c7e00
admin <admin@MacBook-Pro-admin.local>
2019-08-17 16:40:08 +0200

    a

--- execrices/sets.py
+++ execrices/sets.py
@@ -1,13 +1,21 @@
 # Объединение списков. В конце дубликатов быть не должно
 def union(lst1, lst2):
-    pass
+    new_list = lst1.copy()
+    for i in lst2:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
 
 union([1, 2, 3], [2, 3, 4]) # [1, 2, 3, 4]
 union(['a', 'b', 'c', 'd'], [1, 2, 'd', 'a', 'f']) # ['a', 'b', 'c', 'd', 1, 2, 'f']
 
 # Пересечение списков
 def intersection(lst1, lst2):
-    pass
+    n = []
+    for i in lst2:
+        if i in lst1:
+            n.append(i)
+    return n
 
 intersection([1, 2, 3], [2, 3, 4]) # [2, 3]
 intersection(['a', 'b', 'c', 'd'], [1, 2, 'd', 'a', 'f']) # ['a', 'd']
@@ -15,7 +23,11 @@ intersection([1, 2], [3, 4]) # []
 
 # Вычитание список lst2 из lst1
 def difference(lst1, lst2):
-    pass
+    n = []
+    for i in lst1:
+        if i not in lst2:
+            n.append(i)
+    return n
 
 difference([1, 2, 3], [2, 3, 4]) # [1]
 difference(['a', 'b', 'c', 'd'], ['a', 'b']) # ['c', 'd']
@@ -23,8 +35,22 @@ difference([1, 2], [1, 2]) # []
 
 # Обатное пересечение (входят только те элементы, которые есть либо в lst1 либо в lst2, но не в оба массива)
 def xor(lst1, lst2):
-    pass
+    n = lst1.copy()
+    for i in lst2:
+        if i in lst1:
+            n.remove(i)
+    m = lst2.copy()
+    for a in lst1:
+        if a in lst2:
+            m.remove(a)
+    return n+m
 
 
-xor([1, 2, 3], [2, 3, 4]) # [1, 4]
+xor([1, 2, 3], [2, 3, 4]) # [1, 4]    
 xor(['a', 'b'], ['c', 'd']) # ['a', 'b', 'c', 'd']
+
+def xor_alternative(lst1, lst2):
+    u = union(lst1,lst2)
+    n = intersection(lst1,lst2)
+    s = difference (u,n)
+    return s
